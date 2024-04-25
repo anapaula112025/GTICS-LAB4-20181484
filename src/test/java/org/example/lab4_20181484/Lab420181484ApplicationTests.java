455a646362b8e09a78465b89bc8b0f8fe6f66d64
Ana <anapaula.hachi@hotmail.com>
2024-04-24 19:13:24 -0500

    first commit

---
+++ src/test/java/org/example/lab4_20181484/Lab420181484ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab4_20181484;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420181484ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
